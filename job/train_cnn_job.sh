c81e4a2c5e1e37a3f85532c339b72c1b53ab657b
fannnyyy <fanny.badoules@gmail.com>
2025-12-17 23:18:10 +0100

    wrong pytorch cuda version compatibility

--- job/train_cnn_job.sh
+++ job/train_cnn_job.sh
@@ -1,15 +1,12 @@
 #!/bin/bash 
-#SBATCH --job-name=geoguessr
-#SBATCH --partition=gpu_tp
-#SBATCH --time=2:00:00
+#SBATCH --job-name=geoguessr_cnn
+#SBATCH --partition=gpu_prod_night
+#SBATCH --time=10:00:00
 #SBATCH --output=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.out
 #SBATCH --error=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.err
 
-# Charger conda
 module load anaconda3/2022.10/gcc-13.1.0
 eval "$(conda shell.bash hook)"
-
-# Activer l'environnement
 conda activate geoguesseriassh
 
 cd $HOME/GeoGuesserIA/model/cnn
