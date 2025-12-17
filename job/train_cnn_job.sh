45968ccddf7afb78b349244ee8347e7b39266a44
fannnyyy <fanny.badoules@gmail.com>
2025-12-17 23:03:18 +0100

    create conda env + some fails of train

--- job/train_cnn_job.sh
+++ job/train_cnn_job.sh
@@ -2,17 +2,15 @@
 #SBATCH --job-name=geoguessr
 #SBATCH --partition=gpu_tp
 #SBATCH --time=2:00:00
-#SBATCH --output=logslurms/slurm-%j.out
-#SBATCH --error=logslurms/slurm-%j.err
+#SBATCH --output=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.out
+#SBATCH --error=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.err
 
+# Charger conda
 module load anaconda3/2022.10/gcc-13.1.0
-
 eval "$(conda shell.bash hook)"
 
+# Activer l'environnement
 conda activate geoguesseriassh
-pip install scikit-learn torch torchvision pillow numpy pandas matplotlib
-
-
 
 cd $HOME/GeoGuesserIA/model/cnn
 python3 cnn.py
