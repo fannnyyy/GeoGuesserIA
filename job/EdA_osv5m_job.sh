b5d4987f2afb67488237dba4c09ea068b505dc66
fannnyyy <fanny.badoules@gmail.com>
2026-01-15 14:58:51 +0100

    test download dataset osv5m

---
+++ job/EdA_osv5m_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH --job-name=geoguessr
+#SBATCH --partition=gpu_tp
+#SBATCH --time=2:00:00
+#SBATCH --output=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.out
+#SBATCH --error=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.err
+
+
+source ~/.bashrc
+conda activate geoguesseriassh
+
+cd ~/GeoGuesserIA/dataset_OSV5M
+
+python3 EdA.py
