5a2353fb7b7eebc56ba48d6de02b0ac8bb2457f9
fannnyyy <fanny.badoules@gmail.com>
2026-01-16 10:20:28 +0100

    update dataset osv5m

--- job/update_csv_osv5m_job.sh
+++ job/update_csv_osv5m_job.sh
@@ -1,7 +1,7 @@
-#!/bin/bash 
-#SBATCH --job-name=geoguessr
-#SBATCH --partition=gpu_tp
-#SBATCH --time=2:00:00
+#!/bin/bash
+#SBATCH --job-name=update_csv_osv5m
+#SBATCH --partition=gpu_prod_night
+#SBATCH --time=12:00:00
 #SBATCH --output=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.out
 #SBATCH --error=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.err
 
@@ -12,4 +12,4 @@ conda activate geoguesseriassh
 
 cd ~/GeoGuesserIA/dataset_OSV5M
 
-python3 EdA.py
+python3 Update_csv.py
