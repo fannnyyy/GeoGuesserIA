3ef6d107cc859b7e928dcc31da140b920a7164e6
fannnyyy <fanny.badoules@gmail.com>
2026-03-05 12:26:03 +0100

    add some cnn and test

---
+++ job/train_cnn_numpy_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+#SBATCH --job-name=geoguessr
+#SBATCH --partition=gpu_prod_night
+#SBATCH --time=12:00:00
+#SBATCH --output=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.out
+#SBATCH --error=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.err
+
+module load anaconda3/2022.10/gcc-13.1.0
+eval "$(conda shell.bash hook)"
+conda activate geoguesseriassh
+
+cd $HOME/GeoGuesserIA/model/cnn
+python3 cnn_from_scratch_numpy.py
