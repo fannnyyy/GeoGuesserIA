498cd9afd0854e139d62477e813be6b5c1dba087
fannnyyy <fanny.badoules@gmail.com>
2026-03-27 10:44:46 +0100

    some test for finding good path

---
+++ job/train_cnn_full_dataset_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+#SBATCH --job-name=geoguessr
+#SBATCH --partition=gpu_prod_long
+#SBATCH --time=24:00:00
+#SBATCH --output=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.out
+#SBATCH --error=/usr/users/geoguessr_ia/badoul_fan/GeoGuesserIA/logslurms/slurm-%j.err
+
+module load anaconda3/2022.10/gcc-13.1.0
+eval "$(conda shell.bash hook)"
+conda activate geoguesseriassh
+
+cd $HOME/GeoGuesserIA/model/cnn/resnet/resnet_full_classif
+python3 resnet_classification.py
